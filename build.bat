4632c85f5bbc14ed00fa1cd8925ea69c98acc2d0
jonssonyan <yz808@outlook.com>
2024-09-11 11:24:06 +0800

    fix: caddy forward proxy

--- build.bat
+++ build.bat
@@ -1,35 +1,40 @@
 @echo off
-
-:: Install xcaddy if not already installed
-go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
+:: Check if xcaddy is installed
+where xcaddy >nul 2>nul
+if errorlevel 1 (
+    echo xcaddy is not installed, installing...
+    go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
+) else (
+    echo xcaddy is already installed.
+)
 
 :: Define an array of platforms and architectures
 set PLATFORMS=386 amd64 armv6 armv7 arm64 ppc64le s390x
+
+:: Set fixed environment variables
+set GOOS=linux
+set CGO_ENABLED=0
 
 :: Loop through each platform and build
 for %%P in (%PLATFORMS%) do (
     echo Building Caddy for linux/%%P...
 
+    set "GOARM="
     if %%P==armv6 (
-        set GOOS=linux
         set GOARCH=arm
         set GOARM=6
     ) else if %%P==armv7 (
-        set GOOS=linux
         set GOARCH=arm
         set GOARM=7
     ) else (
-        set GOOS=linux
         set GOARCH=%%P
-        set GOARM=
     )
 
-    set CGO_ENABLED=0
     xcaddy build --with github.com/caddyserver/forwardproxy=github.com/klzgrad/forwardproxy@naive --output build/caddy-forwardproxy-%GOOS%-%GOARCH%%GOARM%
-    
+
     if errorlevel 1 (
         echo Failed to build Caddy for %GOOS%/%GOARCH%%GOARM%. Skipping.
     ) else (
-        echo Successfully build: build/caddy-forwardproxy-%GOOS%-%GOARCH%%GOARM%.
+        echo Successfully built: build/caddy-forwardproxy-%GOOS%-%GOARCH%%GOARM%.
     )
 )
